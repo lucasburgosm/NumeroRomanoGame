a25198643caa232915a8ada08cbde0d2b7d9129e
lucasburgosm <montesanolucas@gmail.com>
2022-08-19 12:45:02 -0300

    l

--- numerosRomanos.js
+++ numerosRomanos.js
@@ -1,4 +1,3 @@
-
 let vidas = 3,
     totalIntentos = 10,
     puntos = 0,
@@ -6,7 +5,7 @@ let vidas = 3,
 
 intentos.innerHTML = `Intentos: ${totalIntentos} - Puntos: ${puntos}`
 
-const startgame = () => {console.log("hola")};
+const startgame = () => {console.log("hola puto")};
 document.getElementById("prueba").onclick = () => { startgame()}
 
 
@@ -46,57 +45,51 @@ document.getElementById("game").innerHTML = `Enter! ${vidas} lifes in this numbe
 }
 
 
-
-
-
 function convertToRoman(num) {
-  if(isFinite(num) == false) { return "Number Is not an Integer"  ;}   
-  const numString = `${num}`;
-  let finalNumber = [],
-      cases = 1;
-  for(let i = numString.length - 1; i >= 0 ; i--) {
-  finalNumber = switchLetter(cases, numeroR(numString[i])) + finalNumber;  
-  cases = cases + 1;
-  }
-  return finalNumber;
-  function numeroR(a)  {  
-      if(a == 0){ return ""
-      ;}
-      if(a > 0 && a < 4){ return "I".repeat(a)
-      ;} 
-      else if(a == 4) { return "IV"
-      ;} 
-      else if(a > 4 && a < 9) {
-      return "V" + "I".repeat(a - 5)
-      ;}
-      else if(a == 9) {
-      return "IX"
-      ;}
-      }    
-  function  switchLetter(position,a) { 
-      switch(position) {
-      case 1:
-      break;
-      case 2:
-      a = a.replace(/x/gi,"C");
-      a = a.replace(/i/gi,"X");
-      a = a.replace(/v/gi,"L");
-      break;
-      case 3:
-      a = a.replace(/x/gi,"M");
-      a = a.replace(/i/gi,"C");
-      a = a.replace(/v/gi,"D");
-      break;
-      case 4:
-      a = a.replace(/i/gi,"M");
-      }
-      return a
-      }
-  
-  }
+if(isFinite(num) == false) { return "Number Is not an Integer"  ;}   
+const numString = `${num}`;
+let finalNumber = [],
+    cases = 1;
+for(let i = numString.length - 1; i >= 0 ; i--) {
+finalNumber = switchLetter(cases, numeroR(numString[i])) + finalNumber;  
+cases = cases + 1;
+}
+return finalNumber;
+function numeroR(a)  {  
+    if(a == 0){ return ""
+    ;}
+    if(a > 0 && a < 4){ return "I".repeat(a)
+    ;} 
+    else if(a == 4) { return "IV"
+    ;} 
+    else if(a > 4 && a < 9) {
+    return "V" + "I".repeat(a - 5)
+    ;}
+    else if(a == 9) {
+    return "IX"
+    ;}
+    }    
+function  switchLetter(position,a) { 
+    switch(position) {
+    case 1:
+    break;
+    case 2:
+    a = a.replace(/x/gi,"C");
+    a = a.replace(/i/gi,"X");
+    a = a.replace(/v/gi,"L");
+    break;
+    case 3:
+    a = a.replace(/x/gi,"M");
+    a = a.replace(/i/gi,"C");
+    a = a.replace(/v/gi,"D");
+    break;
+    case 4:
+    a = a.replace(/i/gi,"M");
+    }
+    return a
+    }
 
-
-
+}
 
 
 
